cc1c592b45dc3097ff1d9fb1e291b9646c1250bd
Oliver Wong <oliverwong0301@gmail.com>
2024-01-02 11:34:55 +0700

    input lesson

--- First_Python_Program/hello_world.py
+++ First_Python_Program/hello_world.py
@@ -4,3 +4,17 @@ print ("Hello World!")
 
 print ("Goodbye World!")
 
+new_line = 'Hello \nMr. Tuan'
+print(new_line)
+
+a = "your face"
+b = "my face"
+print(a + " " + b)
+
+# From python 3.6 and above, you can use F-String by adding an "f" before a string to ask it accept interger or other datatype
+
+guess = 10
+print(f"Your booking of {guess} guesses is incorrect")
+
+a = True
+print(f"Everything you say in the court is: {a}")
---
+++ First_Python_Program/input.py
@@ -0,0 +1,8 @@
+print("How many Kilometers did you complete running today?")
+kms = input()
+miles = float(kms) / 1.60934
+print(f"Ok, you've just said {kms}")
+
+# round(thing to round, how many decimal points)
+# or miles = round(miles, 2) then print line just need {miles}
+print (f"And that is equal to {round(miles, 2)}")
